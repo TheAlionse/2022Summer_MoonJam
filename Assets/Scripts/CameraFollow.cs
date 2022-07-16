5262568a2e5e0d82a0891fd5a822a14cdca7cbf4
TheAlionse <55151815+TheAlionse@users.noreply.github.com>
2022-07-16 12:48:03 -0400

    Basic Map Temp

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,12 +6,14 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     public float smooth_time;
+    public float zoom_level;
+
     private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 target_position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        Vector3 target_position = new Vector3(player.transform.position.x, player.transform.position.y, zoom_level);
         transform.position = Vector3.SmoothDamp(transform.position, target_position, ref velocity, smooth_time);
     }
 }
