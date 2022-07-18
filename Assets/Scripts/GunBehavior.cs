9103c8692f0fcf54c0990eccc615b8e250047757
jackazadian1 <44512878+jackazadian1@users.noreply.github.com>
2022-07-18 22:33:11 +0300

    equip gun

--- Assets/Scripts/GunBehavior.cs
+++ Assets/Scripts/GunBehavior.cs
@@ -52,30 +52,65 @@ public class GunBehavior : MonoBehaviour
 
     void SwapGun()
     {
-        Destroy(gunObject);
-        GameObject new_gun;
-        if (special_gun_counter >= 10 && Random.Range(0, 4) == 1)
+        if(guns.Count >= 2)
         {
-            special_gun_counter = 0;
-            new_gun = specialGun;
-        }
-        else
-        {
-            int new_gun_index = Random.Range(0, guns.Count);
-            while (last_gun_index == new_gun_index)
+            Destroy(gunObject);
+            GameObject new_gun;
+
+            if (special_gun_counter >= 10 && Random.Range(0, 4) == 1)
             {
-                new_gun_index = Random.Range(0, guns.Count);
+                special_gun_counter = 0;
+                new_gun = specialGun;
             }
-            new_gun = guns[new_gun_index];
-            last_gun_index = new_gun_index;
-            special_gun_counter++;
+            else
+            {
+                int new_gun_index = Random.Range(0, guns.Count);
+                while (last_gun_index == new_gun_index)
+                {
+                    new_gun_index = Random.Range(0, guns.Count);
+                }
+                new_gun = guns[new_gun_index];
+                last_gun_index = new_gun_index;
+                if(specialGun != null)
+                {
+                    special_gun_counter++;
+                }
+            }
+            EquipGun(new_gun);
         }
+        
+    }
+
+    public void AddGun(GameObject gun_prefab)
+    {
+        guns.Add(gun_prefab);
+        if(gunObject != null)
+        {
+            Destroy(gunObject);
+        }
+        last_gun_index = guns.Count - 1;
+        EquipGun(gun_prefab);
+    }
+
+    public void AddSpecialGun(GameObject gun_prefab)
+    {
+        specialGun = gun_prefab;
+        if (gunObject != null)
+        {
+            Destroy(gunObject);
+        }
+        last_gun_index = -1;
+        EquipGun(gun_prefab);
+
+    }
+
+    void EquipGun(GameObject new_gun)
+    {
         gunObject = Instantiate(new_gun, transform.position, Quaternion.identity);
         gunObject.transform.parent = gameObject.transform.parent.transform;
         gunStats = gunObject.GetComponent<GunStats>();
 
-        Debug.Log(gunObject.name);
-        if(gunObject.name == "cumbee(Clone)")
+        if (gunObject.name == "cumbee(Clone)")
         {
             gunObject.transform.eulerAngles = new Vector3(180, 180, 0);
         }
@@ -88,15 +123,6 @@ public class GunBehavior : MonoBehaviour
             gunObject.transform.localScale = new Vector3(-gunObject.transform.localScale.x, gunObject.transform.localScale.y, gunObject.transform.localScale.z);
         }
         cooldown_timestamp = Time.time;
-    }
 
-    public void AddGun(GameObject gun_prefab)
-    {
-        guns.Add(gun_prefab);
-    }
-
-    public void AddSpecialGun(GameObject gun_prefab)
-    {
-        specialGun = gun_prefab;
     }
 }
