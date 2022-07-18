cadb9496b8ba936a32a9bda3b8a2846ba6801771
TheAlionse <55151815+TheAlionse@users.noreply.github.com>
2022-07-18 19:19:43 -0400

    Updated hp

--- Assets/Scripts/HouseInteract.cs
+++ Assets/Scripts/HouseInteract.cs
@@ -56,7 +56,7 @@ public class HouseInteract : MonoBehaviour
             {
                 scream.Play();
             }
-            player.GetComponent<PlayerHealth>().heal_player(100);
+            player.GetComponent<PlayerHealth>().heal_player(1000);
             player.GetComponent<PlayerMoveTest>().respawn_point = player.transform.position;
             Debug.Log(player.GetComponent<PlayerMoveTest>().respawn_point);
         }
